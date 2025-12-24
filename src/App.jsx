901c6313a3e2e49011cc4d9cc21967353d810a99
Rayon-313 <maharjan.rayon313@gmail.com>
2025-12-24 19:12:37 +0545

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/frontend/Home.JSX'
+import Home from './pages/frontend/Home'
 import About from './pages/frontend/About'
 import Contact from './pages/frontend/Contact'
 import Category from './pages/frontend/Category'
